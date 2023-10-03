7841c9f859567f9caa5032bb581347cfe0dbdc4e
Fjodormelehhov <fjodor.melehhov@tktk.ee>
2023-10-03 20:23:37 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal valida tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema nime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr. [kasutaja nimi]" / "Welcome, Ms. [kasutaja nimi]"
+
+
+Console.WriteLine("Please,select your gender (m/f)");
+
+char usergender = Char.Parse( Console.ReadLine()); //loeb konsoolist maha andmeid string formaadis
+
+
+Console.WriteLine("Please, enter your last name: ") ;
+string username = Console.ReadLine();
+
+
+
+if (usergender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {username}!");
+}
+else if (usergender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {username}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {username}! ");
+}
---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal sisestada tema vanuse
+//kui kasutaja vanus on võiksem kui 13, siis konsoolis kuvatakse
+//"You are too young to use instagram"
+//muul juhul
+//Konsoolis kuvatakse "Welcome to instagram"
+
+using System.Diagnostics;
+
+Console.WriteLine("Enter your age: ");
+
+string userage = Console.ReadLine();
+
+int useragenum = 0;
+
+//boolean - true/false
+
+bool isagenumber = Int32.TryParse(userage, out useragenum);
+
+//int userage = Int32.Parse(Console.ReadLine()); //"13" - heap, 13 stack
+
+
+
+if (isagenumber)
+{
+    if (useragenum >= 13)
+    {
+        Console.WriteLine("Welcome to instagram!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use instagram.");
+    }
+} 
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
